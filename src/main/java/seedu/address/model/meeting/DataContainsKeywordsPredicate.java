a4528dd424787cb64990603dacb1ef1d80f18bbb
KOH-ZHE-HAO <lincolnkoh99@gmail.com>
2020-10-13 17:58:03 +0800

    Bug fix DataContainsKeywordsPredicate.java

---
+++ src/main/java/seedu/address/model/meeting/DataContainsKeywordsPredicate.java
@@ -0,0 +1,41 @@
+package seedu.address.model.meeting;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code Meetings}'s {@code data} matches any of the keywords given.
+ */
+public class DataContainsKeywordsPredicate implements Predicate<Meeting> {
+    private final List<String> keywords;
+
+    public DataContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Meeting meeting) {
+        return keywords.stream()
+                .anyMatch(keyword -> {
+                    final StringBuilder builder = new StringBuilder();
+                    builder.append(meeting.getTitle())
+                            .append(meeting.getDateTime())
+                            .append(meeting.getDuration())
+                            .append(meeting.getLocation());
+
+                    meeting.getParticipants().forEach(builder::append);
+                    return builder
+                            .toString()
+                            .toLowerCase()
+                            .contains(keyword.toLowerCase());
+                });
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DataContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((DataContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
